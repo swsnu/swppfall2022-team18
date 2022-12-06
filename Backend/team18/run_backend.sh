7c7cd4064bf39accc9aadcbfcf5d7cf933419304
Ubuntu <ubuntu@ip-172-31-39-105.ap-northeast-2.compute.internal>
2022-12-06 18:13:58 +0000

    keras

--- Backend/team18/run_backend.sh
+++ Backend/team18/run_backend.sh
@@ -1,8 +1,10 @@
 pip install djangorestframework==3.14.0
 pip install Pillow==8.4
+pip install tensorflow
+pip install keras
 # python manage.py makemigrations
 # python manage.py migrate
 # python manage.py loaddata final.json
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
 mkdir -p /log # for `uwsgi` logging
 # uwsgi --ini uwsgi/uwsgi.ini
--- run_docker_backend.sh
+++ run_docker_backend.sh
@@ -1,14 +1,18 @@
 ABSOLUTE_DB_PATH="/home/ubuntu/swppfall2022-team18/Backend/team18/db.sqlite3"
 BACKEND_CONTAINER_DB_PATH="/app/db.sqlite3"
 
+ABSOLUTE_MEDIA_PATH="/home/ubuntu/swppfall2022-team18/Backend/team18/media"
+BACKEND_CONTAINER_MEDIA_PATH="/app/media"
+
 # sudo docker run -d --rm \
 #     --name "backend" \
 #     -p 8000:8000 \
 #     -v $ABSOLUTE_DB_PATH:$BACKEND_CONTAINER_DB_PATH \
 #     backend:latest \
 
-sudo docker run --rm \
+sudo docker run --rm -it \
 --name "backend" \
 -p 8000:8000 \
 -v $ABSOLUTE_DB_PATH:$BACKEND_CONTAINER_DB_PATH \
-backend:latest
+-v $ABSOLUTE_MEDIA_PATH:$BACKEND_CONTAINER_MEDIA_PATH \
+backend:latest /bin/bash
